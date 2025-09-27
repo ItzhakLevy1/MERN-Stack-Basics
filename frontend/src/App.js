bac72ab46671710a9e7613112acc69b61e83207d
Itzhak Levy <itzhaklevydev@gmail.com>
2025-09-27 21:10:45 +0300

    feat(router): setup React Router with Home page and Navbar
    - installed react-router-dom
    - added BrowserRouter, Routes, and Route in App.js
    - created Home page component
    - created Navbar component with link to '/'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 // The BrowserRouter wraps the entire application enabling routing, Routes and Route components are used to define the app's routes
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Pages & components
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
       {/* Every thing that needs to use the routing system needs to be inside <BrowserRouter> */}
       <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   );
